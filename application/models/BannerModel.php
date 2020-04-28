8fa2e6c86dd600f7d169e203ea46ba982d3ea510
changyouquan <492441479@qq.com>
2020-04-28 17:37:17 +0800

    1

--- application/models/BannerModel.php
+++ application/models/BannerModel.php
@@ -16,7 +16,7 @@ class BannerModel extends Crud {
     {
         $result = [
             [
-                'img' => httpurl('/static/img/pic11.jpg'),
+                'img' => httpurl('/static/img/pic12.jpg'),
                 'title' => ''
             ]
         ];
